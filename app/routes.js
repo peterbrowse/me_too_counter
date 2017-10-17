3f4686f6e9661ee24fb4069f6513a9f05e68a22a
Peter Browse <peter.browse@llm-pbro-1534uk.wk.com>
2017-10-17 21:58:29 +0100

    Stripping project back

--- app/routes.js
+++ app/routes.js
@@ -3,11 +3,8 @@ var functions 	= require('./functions');
 
 module.exports = function(app) {
 	app.get('/', function (req, res) {
-		var number_of_sounds = 18;
-		
 		res.render('index', {
-			title: "Lurpak - Soundboard",
-			number_of_sounds: number_of_sounds
+			title: "#METOO Counter"
 		});
 	});
 }
